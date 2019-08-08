eb4a26c500311720a5d628223527cb8c213348e9
cristinaRaimondo <ochocaracteres.raimondo1@gmail.com>
2019-08-08 04:37:02 -0300

    nueva intro

--- islasAndaman/src/player/Personaje.js
+++ islasAndaman/src/player/Personaje.js
@@ -1,83 +1,78 @@
 class Personaje extends Phaser.GameObjects.Sprite {
-  constructor(config) {
-      super(config.scene, config.x, config.y, 'personaje');
+    constructor(config) {
+        super(config.scene, config.x, config.y, 'personaje');
 
-      this.scene = config.scene;
-      this.scene.add.existing(this);
-      this.scene.physics.world.enable(this);
+        this.scene = config.scene;
+        this.scene.add.existing(this);
+        this.scene.physics.world.enable(this);
 
-      this.setScale(2);
-      this.body.setSize(14, 20);
-      this.body.setOffset(2, 5);
-      this.body.setBounce(0.2);
+        this.setScale(2);
+        this.body.setSize(14, 20);
+        this.body.setOffset(2, 5);
+        this.body.setBounce(0.2);
 
-      this.jumping = false;
+        this.jumping = false;
 
-      this.anims.play('rubioidle');
-      this.prevMov = 'rubioidle';
+        this.anims.play('rubioidle');
+        this.prevMov = 'rubioidle';
 
-      this.hitDelay = false;
+        this.hitDelay = false;
 
-      this.cursor = this.scene.input.keyboard.createCursorKeys();
+        this.cursor = this.scene.input.keyboard.createCursorKeys();
 
-      this.life = 3;
+        this.life = 3;
+    }
 
-  }
+    update() {
+        if (this.cursor.left.isDown) {
+            this.body.setVelocityX(-200);
+            this.flipX = true;
+            if (this.prevMov !== 'left' && !this.jumping) {
+                this.prevMov = 'left';
+                this.anims.play('rubioizq');
+            }
+        } else if (this.cursor.right.isDown) {
+            this.body.setVelocityX(200);
+            this.flipX = false;
+            if (this.prevMov !== 'right' && !this.jumping) {
+                this.prevMov = 'right';
+                this.anims.play('rubioder');
+            }
+        } else if (this.cursor.down.isDown && !this.jumping) {
+            this.body.setVelocityX(0);
+            this.body.setSize(14, 15);
+            this.body.setOffset(2, 10);
 
-  update() {
-      if(this.cursor.left.isDown) {
-          this.body.setVelocityX(-200);
-          this.flipX = true;
-          if(this.prevMov !== 'left' && !this.jumping) {
-              this.prevMov = 'left';
-              this.anims.play('rubioizq');
-          }
-      } else if(this.cursor.right.isDown) {
-          this.body.setVelocityX(200);
-          this.flipX = false;
-          if(this.prevMov !== 'right' && !this.jumping) {
-              this.prevMov = 'right';
-              this.anims.play('rubioder');
-          }
+            if (this.prevMov !== 'down' && !this.jumping) {
+                this.prevMov = 'down';
+                this.anims.play('rubioidle');
+            }
+        } else {
+            this.body.setVelocityX(0);
+            this.body.setSize(14, 20);
+            this.body.setOffset(2, 5);
+            if (this.prevMov !== 'rubioidle' && !this.jumping) {
+                this.prevMov = 'rubioidle';
+                this.anims.play('rubioidle');
+            }
+        }
 
-      } else if(this.cursor.down.isDown && !this.jumping) {
-          this.body.setVelocityX(0);
-          this.body.setSize(14, 15);
-          this.body.setOffset(2, 10);
+        if (Phaser.Input.Keyboard.JustDown(this.cursor.up) && !this.jumping) {
+            this.jumping = true;
+            this.body.setVelocityY(-800);
+            if (this.prevMov !== 'jump') {
+                this.prevMov = 'jump';
+                this.anims.play('rubioder');
+            }
+        } else if (this.body.blocked.down) {
+            this.jumping = false;
+        }
+    }
 
-          if(this.prevMov !== 'down' && !this.jumping) {
-              this.prevMov = 'down';
-              this.anims.play('rubioidle');
-          }
-
-      }
-      else {
-          this.body.setVelocityX(0);
-          this.body.setSize(14, 20);
-          this.body.setOffset(2, 5);
-          if(this.prevMov !== "rubioidle" && !this.jumping) {
-              this.prevMov = "rubioidle";
-              this.anims.play("rubioidle");
-          }
-      }
-
-      if(Phaser.Input.Keyboard.JustDown(this.cursor.up) && !this.jumping) {
-          this.jumping = true;
-          this.body.setVelocityY(-800);
-          if(this.prevMov !== 'jump') {
-              this.prevMov = 'jump';
-              this.anims.play('rubioder');
-          }
-      } else if(this.body.blocked.down) {
-          this.jumping = false;
-      }
-  }
-
-  bombCollision() {
-      if(!this.hitDelay) {
-
-      }
-  }
+    bombCollision() {
+        if (!this.hitDelay) {
+        }
+    }
 }
 
 export default Personaje;
--- islasAndaman/src/scenes/selva.js
+++ islasAndaman/src/scenes/selva.js
@@ -13,12 +13,8 @@ class Selva extends Phaser.Scene {
 
     create() {
         this.boton = this.add.image(400, 300, 'pelota').setScale(1);
-        // this.scene.launch('Anara');
-        // this.scene.launch('Ivan');
-        // this.scene.launch('Kelen');
+
         this.currentScene = this.scene.get(this);
-        // this.marron = this.add.sprite(500, 500, 'marron', 1).setScale(2);
-        // this.marron.anims.play('frente');
 
         this.pointsText = this.add
             .bitmapText(600, 100, 'font', 'PUNTOS ' + this.points)
